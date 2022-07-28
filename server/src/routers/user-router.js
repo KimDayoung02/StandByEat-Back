fd31d629706447c7d32fe5072554e54d1a2c91ce
TaeHun <ekdh0858@naver.com>
2022-07-28 23:40:35 +0900

    fix : 프로필 수정 시 비밀번호 사용 안함

--- server/src/routers/user-router.js
+++ server/src/routers/user-router.js
@@ -1,15 +1,15 @@
-import { Router } from 'express';
-import is from '@sindresorhus/is';
+import { Router } from "express";
+import is from "@sindresorhus/is";
 import {
   loginRequired,
   errorHandler,
   adminRequired,
   registerCheck,
-} from '../middlewares';
-import { userService } from '../services';
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-const aws = require('aws-sdk');
+} from "../middlewares";
+import { userService } from "../services";
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
 
 const userRouter = Router();
 
@@ -22,8 +22,8 @@ const s3 = new aws.S3({
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'standbyeat', // 버킷 이름 입력
-    acl: 'public-read',
+    bucket: "standbyeat", // 버킷 이름 입력
+    acl: "public-read",
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
       cb(null, `user/${Date.now()}_${file.originalname}}`);
@@ -33,13 +33,13 @@ const upload = multer({
 
 // 회원가입 api (아래는 /register이지만, 실제로는 /api/register로 요청해야 함.)
 userRouter.post(
-  '/register',
+  "/register",
   registerCheck,
   async (req, res, next) => {
     try {
       if (is.emptyObject(req.body)) {
         throw new Error(
-          'headers의 Content-Type을 application/json으로 설정해주세요'
+          "headers의 Content-Type을 application/json으로 설정해주세요"
         );
       }
 
@@ -67,12 +67,12 @@ userRouter.post(
 
 // 로그인 api (아래는 /login 이지만, 실제로는 /api/login로 요청해야 함.)
 userRouter.post(
-  '/login',
+  "/login",
   async function (req, res, next) {
     try {
       if (is.emptyObject(req.body)) {
         throw new Error(
-          'headers의 Content-Type을 application/json으로 설정해주세요'
+          "headers의 Content-Type을 application/json으로 설정해주세요"
         );
       }
 
@@ -90,36 +90,24 @@ userRouter.post(
 
 // 사용자 정보 수정
 userRouter.patch(
-  '/update/:userId',
+  "/update/:userId",
   loginRequired,
-  upload.single('profileImgUrl'),
+  upload.single("profileImgUrl"),
   async function (req, res, next) {
     try {
       if (is.emptyObject(req.body)) {
         throw new Error(
-          'headers의 Content-Type을 application/json으로 설정해주세요'
+          "headers의 Content-Type을 application/json으로 설정해주세요"
         );
       }
 
       const userId = req.params.userId;
       const profileImgUrl = req.file?.location;
 
-      const {
-        name,
-        phoneNumber,
-        nickName,
-        location,
-        birth,
-        gender,
-        pw,
-        currentPassword,
-      } = req.body;
+      const { name, phoneNumber, nickName, location, birth, gender, pw } =
+        req.body;
 
-      if (!currentPassword) {
-        throw new Error('정보를 변경하려면, 현재의 비밀번호가 필요합니다.');
-      }
-
-      const userInfoRequired = { userId, currentPassword };
+      const userInfoRequired = { userId };
 
       const toUpdate = {
         ...(name && { name }),
@@ -147,7 +135,7 @@ userRouter.patch(
 
 // 사용자 삭제
 userRouter.delete(
-  '/delete',
+  "/delete",
   async function (req, res, next) {
     try {
       const { userId, userPassword } = req.body;
@@ -162,7 +150,7 @@ userRouter.delete(
 
 // 모든 유저 정보를 가져옴
 userRouter.get(
-  '/users',
+  "/users",
   // adminRequired,
   async function (req, res, next) {
     try {
@@ -177,7 +165,7 @@ userRouter.get(
 
 // 유저 한명의 정보를 가져온다.
 userRouter.get(
-  '/:userId',
+  "/:userId",
   loginRequired,
   async function (req, res, next) {
     try {
@@ -193,7 +181,7 @@ userRouter.get(
 
 // 유저 한명의 정보를 가져온다.
 userRouter.delete(
-  '/delete/:userObi',
+  "/delete/:userObi",
   adminRequired,
   async function (req, res, next) {
     try {
@@ -209,7 +197,7 @@ userRouter.delete(
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // 카카오 회원가입
-userRouter.post('/register/kakao', async (req, res, next) => {
+userRouter.post("/register/kakao", async (req, res, next) => {
   try {
     const email = req.body.email;
     const nickname = req.body.nickname;
@@ -222,7 +210,7 @@ userRouter.post('/register/kakao', async (req, res, next) => {
   }
 });
 // 카카오 로그인
-userRouter.post('/login/kakao', async function (req, res, next) {
+userRouter.post("/login/kakao", async function (req, res, next) {
   try {
     const email = req.body.email;
 
--- server/src/services/user-service.js
+++ server/src/services/user-service.js
@@ -1,7 +1,7 @@
-import { userModel } from '../db';
+import { userModel } from "../db";
 
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 class UserService {
   constructor(userModel) {
@@ -24,7 +24,7 @@ class UserService {
 
     if (user) {
       throw new Error(
-        '이 아이디는 현재 사용중입니다. 다른 아이디를 입력해 주세요.'
+        "이 아이디는 현재 사용중입니다. 다른 아이디를 입력해 주세요."
       );
     }
 
@@ -55,7 +55,7 @@ class UserService {
     let user = await this.userModel.findUserById(id);
     if (!user) {
       throw new Error(
-        '해당 아이디로 가입 내역이 없습니다. 다시 한번 확인해 주세요.'
+        "해당 아이디로 가입 내역이 없습니다. 다시 한번 확인해 주세요."
       );
     }
 
@@ -63,12 +63,12 @@ class UserService {
     const isPasswordCorrect = await bcrypt.compare(pw, correctPasswordHash);
 
     if (!isPasswordCorrect) {
-      throw new Error('비밀번호가 일치하지 않습니다. 다시 한번 확인해 주세요.');
+      throw new Error("비밀번호가 일치하지 않습니다. 다시 한번 확인해 주세요.");
     }
 
-    const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
+    const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
     const token = jwt.sign(
-      { id: id, userId: user._id, role: 'user' },
+      { id: id, userId: user._id, role: "user" },
       secretKey
     );
     return { token };
@@ -76,31 +76,12 @@ class UserService {
 
   // 유저정보 수정하기, 현재 비밀번호가 있어야 수정 가능함.
   async setUser(userInfoRequired, toUpdate) {
-    const { userId, currentPassword } = userInfoRequired;
+    const { userId } = userInfoRequired;
 
     let user = await this.userModel.findUserById(userId);
 
     if (!user) {
-      throw new Error('가입 내역이 없습니다. 다시 한 번 확인해 주세요.');
-    }
-
-    const correctPasswordHash = user.pw;
-    const isPasswordCorrect = await bcrypt.compare(
-      currentPassword,
-      correctPasswordHash
-    );
-
-    if (!isPasswordCorrect) {
-      throw new Error(
-        '현재 비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.'
-      );
-    }
-
-    const { pw } = toUpdate;
-
-    if (pw) {
-      const newPasswordHash = await bcrypt.hash(pw, 10);
-      toUpdate.pw = newPasswordHash;
+      throw new Error("가입 내역이 없습니다. 다시 한 번 확인해 주세요.");
     }
 
     user = await this.userModel.updateUserInfo(userId, toUpdate);
@@ -119,7 +100,7 @@ class UserService {
 
     if (!isPasswordCorrect) {
       throw new Error(
-        '현재 비밀번호가 일치하지 않습니다. 다시 한번 확인해 주세요.'
+        "현재 비밀번호가 일치하지 않습니다. 다시 한번 확인해 주세요."
       );
     }
 
@@ -134,7 +115,7 @@ class UserService {
     let user = await this.userModel.findUserByOId(_id);
 
     if (!user) {
-      throw new Error('이미 없는 사용자 입니다.');
+      throw new Error("이미 없는 사용자 입니다.");
     }
     const userId = user.id;
     // db에 저장
@@ -161,19 +142,19 @@ class UserService {
   // 카카오 회원의 정보 (이메일-> 아이디)
   async getUserTokenWithKakao(email) {
     if (!email) {
-      throw new Error('로그인을 위해서는 이메일 필요합니다');
+      throw new Error("로그인을 위해서는 이메일 필요합니다");
     }
 
     const user = await this.userModel.findUserById(email);
     if (!user) {
       throw new Error(
-        '해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.'
+        "해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요."
       );
     }
 
-    const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
+    const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
     const token = jwt.sign({ userId: user.id, role: user.role }, secretKey);
-    const role = 'user';
+    const role = "user";
 
     return { token, role };
   }
@@ -181,24 +162,24 @@ class UserService {
   // 카카오 Oauth 회원가입
   async addUserWithKakao(email, nickname) {
     if (!email || !nickname) {
-      throw new Error('회원가입을 위해서는 이메일과 이름이 필요합니다');
+      throw new Error("회원가입을 위해서는 이메일과 이름이 필요합니다");
     }
 
     const user = await this.userModel.findUserById(email);
     if (user) {
       throw new Error(
-        '이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.'
+        "이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요."
       );
     }
 
-    const password = 'kakao';
+    const password = "kakao";
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUserInfo = {
       id: email,
       name: nickname,
       password: hashedPassword,
-      phoneNumber: '010-1111-2222',
+      phoneNumber: "010-1111-2222",
     };
 
     const createdNewUser = await this.userModel.createUserByUserInfo(
