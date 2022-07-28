f027c325c2df8cb150489dd7b946f7384bf149ee
GyeongChanOH <dhrudcks01@gmail.com>
2022-07-28 21:13:33 +0900

    fix: 메뉴 이미지 업로드 수정

--- server/src/routers/menu-router.js
+++ server/src/routers/menu-router.js
@@ -36,7 +36,7 @@ menuRouter.post('/menu', upload.single('picture'), async (req, res, next) => {
     // req (request)의 body 에서 데이터 가져오기
     const { storeId, menuName, price, details } = req.body;
 
-    const picture = req.file.location;
+    const picture = req.file?.location;
 
     // 위 데이터를 유저 db에 추가하기
     const newMenu = await menuService.addMenu({
